07e224965294d7c1a0a3ebe7ffe744669b114144
Vinicius Wurzel <vinicius@wurzel.com.br>
2021-06-16 17:26:32 -0300

    Task app lessons code

--- task-app/src/index.js
+++ task-app/src/index.js
@@ -8,6 +8,35 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((err) => {
+        res.status('500').send()
+    })
+})
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+    User.findById(id).then((user) => {
+        res.send(user)
+    }).catch((err) => {
+        res.status('500').send()
+    })
+})
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((err) => {
+        res.status('500').send()
+    })
+})
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id).then((task) => {
+        res.send(task)
+    }).catch((err) => {
+        res.status('404').send()
+    })
+})
 app.post('/users', (req, res) => {
     const user = new User(req.body)
 
@@ -30,3 +59,27 @@ app.post('/tasks', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
+
+// const updateAgeAndCount = async (id, age) => {
+//     const user = await User.findByIdAndUpdate(id, {age})
+//     const count = await User.countDocuments({age})
+//     return count
+// }
+
+// updateAgeAndCount('60c944061a976836512a025a', 2).then((count) => {
+//     console.log(count)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+// const deleteAndCountTasks = async (id) => {
+//     const deleted = await Task.findByIdAndDelete(id)
+//     const countIncompleteTasks = Task.countDocuments({completed: false})
+//     return countIncompleteTasks
+// }
+
+// deleteAndCountTasks('60c9471cbdb70f36f28b7b32').then((count) => {
+//     console.log(count)
+// }).catch((err) => {
+//     console.log(err)
+// })
